379d5bb60361d23bef30ad7b8ce57005c15cc1ca
Dustin <dyelland@gmail.com>
2017-07-27 13:23:54 -0500

    explained the functionality of repos.requestReops

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -6,6 +6,7 @@ var app = app || {};
   repos.all = [];
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // This function is making a request to /github/user/repos, then setting the return value as repos.all in the Promise function, then calls the callback supplied by the function call
   repos.requestRepos = function(callback) {
     $.get('/github/user/repos')
     .then(data => repos.all = data, err => console.error(err))
