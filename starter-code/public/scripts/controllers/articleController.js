e3a336ecb7d8ceaf9430d6e0cd02f594cfe8875d
Dustin <dyelland@gmail.com>
2017-07-27 13:58:32 -0500

    answered mor comment questions

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -4,8 +4,8 @@ var app = app || {};
 (function(module) {
   const articleController = {};
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
-  // (put your response in a comment here)
+  // COM/MENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // articleController.index is being passed an argument `ctx` which is the articles, and passes `ctx.articles` into app.articleView.index
   articleController.index = (ctx) => app.articleView.index(ctx.articles);
 
   // REVIEW: Middleware for grabbing one article by ID:
@@ -15,8 +15,9 @@ var app = app || {};
       next();
     };
 
-    // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
-    // (put your response in a comment here)
+    // COM/MENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+    // This call is making a get request /articles/find and setting the field to article_id nad the value to ctx.params.article_id, then the callback function articleData which sets ctx.articles to article(which is passed in as a parameter)
+    //This function is being called here, it calls tbe articleData function which lives withing this same parent function that we are in(just above).
     app.Article.findWhere('article_id', ctx.params.article_id, articleData);
   };
 
